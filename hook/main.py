4071ac7820b8e6fd459fb4b10c7b3f7fe752cabd
SergioSim <Sergio.Simonian@etu.unice.fr>
2023-10-05 22:45:59 +0200

    ✨(hook) add courses and quiz routes
    We query the Moodle LMS for informations about courses and
    quizzes.

--- hook/main.py
+++ hook/main.py
@@ -1,7 +1,9 @@
 """Hook API main entrypoint."""
 
 import os
+import re
 from contextlib import asynccontextmanager
+from urllib.parse import urlparse
 
 import httpx
 from fastapi import FastAPI, Request
@@ -9,24 +11,170 @@ from fastapi import FastAPI, Request
 
 @asynccontextmanager
 async def lifespan(fastapi_app: FastAPI):
-    """Add moodle client to the FastAPI app at startup."""
+    """Add moodle clients to the FastAPI app at startup."""
+    moodle_url = os.environ.get("HOOK_MOODLE_URL")
+    token = os.environ.get("HOOK_MOODLE_WEBSERVICE_TOKEN")
     fastapi_app.moodle = httpx.AsyncClient(
-        base_url=os.environ.get("HOOK_MOODLE_URL") + "/webservice/rest/server.php",
-        params={
-            "wstoken": os.environ.get("HOOK_MOODLE_WEBSERVICE_TOKEN"),
-            "moodlewsrestformat": "json",
-        },
+        base_url=f"{moodle_url}/webservice/rest/server.php",
+        params={"wstoken": token, "moodlewsrestformat": "json"},
     )
+    fastapi_app.moodle_file = httpx.AsyncClient(
+        base_url=f"{moodle_url}/webservice/pluginfile.php", params={"token": token}
+    )
+
     yield
+
     await fastapi_app.moodle.aclose()
+    await fastapi_app.moodle_file.aclose()
 
 
 app = FastAPI(lifespan=lifespan)
 
 
+def patch_moodle_url(url: str) -> str:
+    """Replace the hostname in `url` with the `HOOK_MOODLE_URL` value."""
+    if not url:
+        return None
+
+    moodle_url = urlparse(os.environ.get("HOOK_MOODLE_URL"))
+    moodle_netloc = moodle_url.netloc
+    moodle_scheme = moodle_url.scheme
+    return urlparse(url)._replace(netloc=moodle_netloc, scheme=moodle_scheme).geturl()
+
+
 @app.get("/")
-async def root(request: Request):
-    """Query a Moodle webservice and return it's response."""
+async def root(request: Request, raw: bool = False):
+    """Get Moodle site info."""
     params = {"wsfunction": "core_webservice_get_site_info"}
-    response = await request.app.moodle.get("/", params=params)
-    return response.json()
+    result = (await request.app.moodle.post("/", params=params)).json()
+    if raw:
+        return result
+
+    return {
+        "sitename": result.get("sitename"),
+        "siteurl": patch_moodle_url(result.get("siteurl")),
+    }
+
+
+@app.get("/courses")
+async def courses(request: Request, raw: bool = False):
+    """Get the list of visible Moodle courses."""
+    params = {"wsfunction": "core_course_get_courses"}
+    result = (await request.app.moodle.post("/", params=params)).json()
+    if raw:
+        return result
+
+    return [
+        {
+            "id": course.get("id"),
+            "fullname": course.get("fullname"),
+            "url": patch_moodle_url(f"/course/view.php?id={course.get('id')}"),
+            "summary": course.get("summary"),
+        }
+        for course in result
+        if course.get("visible") and course.get("format") != "site"
+    ]
+
+
+@app.get("/courses/{course_id}")
+async def course(request: Request, course_id: int, raw: bool = False):
+    """Get the list of visible Moodle course modules by `course_id`."""
+    params = {
+        "wsfunction": "core_course_get_contents",
+        "courseid": course_id,
+    }
+    result = (await request.app.moodle.post("/", params=params)).json()
+    if raw:
+        return result
+
+    get_file = request.app.moodle_file.post
+    return [
+        {
+            "id": module.get("id"),
+            "instance": module.get("instance"),
+            "name": module.get("name"),
+            "modname": module.get("modname"),
+            "url": patch_moodle_url(module.get("url")),
+            "contents": [
+                {
+                    "type": content.get("type"),
+                    "mimetype": content.get("mimetype", "text/html"),
+                    "fileurl": patch_moodle_url(content["fileurl"])
+                    if content.get("type") == "file"
+                    else content["fileurl"],
+                    "content": (
+                        await get_file(patch_moodle_url(content["fileurl"]))
+                    ).text
+                    if content.get("type") == "file"
+                    and content.get("mimetype", "text/html").startswith("text")
+                    else None,
+                }
+                for content in module.get("contents", [])
+                if content.get("type") != "content" and content.get("fileurl")
+            ]
+            if module.get("modname") != "quiz"
+            else (await quiz(request, int(module.get("instance")))),
+        }
+        for section in result
+        for module in section.get("modules", [])
+        if (
+            section.get("visible")
+            and module.get("visible")
+            and module.get("modname") != "label"
+        )
+    ]
+
+
+@app.get("/quiz/{quiz_id}")
+async def quiz(request: Request, quiz_id: int, raw: bool = False):
+    """Get Moodle quiz contents by `quiz_id`."""
+    params = {
+        "wsfunction": "mod_quiz_get_user_attempts",
+        "quizid": quiz_id,
+        "status": "all",
+        "includepreviews": 1,
+    }
+    attempts = (await request.app.moodle.post("/", params=params)).json()
+    # Try to retrieve existing attempt.
+    attempt = next(
+        (x["id"] for x in attempts.get("attempts", []) if x["state"] == "inprogress"),
+        None,
+    )
+
+    # Create new attempt if no previous attempt exists.
+    if not attempt:
+        params = {
+            "wsfunction": "mod_quiz_start_attempt",
+            "quizid": quiz_id,
+        }
+        # return (await request.app.moodle.post("/", params=params)).json()
+        attempt = (await request.app.moodle.post("/", params=params)).json()["attempt"][
+            "id"
+        ]
+
+    # Submit attempt.
+    params = {
+        "wsfunction": "mod_quiz_process_attempt",
+        "attemptid": attempt,
+        "finishattempt": 1,
+    }
+    (await request.app.moodle.post("/", params=params)).json()
+
+    # Get attempt result.
+    params = {
+        "wsfunction": "mod_quiz_get_attempt_review",
+        "attemptid": attempt,
+    }
+    result = (await request.app.moodle.post("/", params=params)).json()
+    if raw:
+        return result
+    pattern = r"<!\[CDATA\[(.*?)\]\]>"
+    return [
+        {
+            "slot": question.get("slot"),
+            "type": question.get("type"),
+            "page": question.get("page"),
+            "html": re.sub(pattern, "", question.get("html"), flags=re.DOTALL),
+        }
+        for question in result.get("questions", [])
+    ]
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,9 +3,106 @@
 import pytest
 from httpx import AsyncClient
 
+from hook.main import patch_moodle_url
+
+COURSES_COUNT = 3
+
 
 @pytest.mark.anyio
 async def test_main_root(client: AsyncClient):
     """Test the main root route."""
-    response = await client.get("/")
-    assert response.json().get("username") == "admin"
+    response = (await client.get("/")).json()
+    assert response == {"sitename": "Hook", "siteurl": "http://moodle"}
+
+
+@pytest.mark.anyio
+async def test_main_root_with_raw(client: AsyncClient):
+    """Test the main root route with `raw=1`."""
+    response = (await client.get("/?raw=1")).json()
+    assert response["username"] == "admin"
+
+
+@pytest.mark.anyio
+async def test_main_courses(client: AsyncClient):
+    """Test the courses route."""
+    response = (await client.get("/courses")).json()
+    assert len(response) == COURSES_COUNT
+    assert response[1] == {
+        "id": 3,
+        "fullname": "Digital Literacy ",
+        "url": "http://moodle/course/view.php?id=3",
+        "summary": (
+            "<p>Introducing the concept of Digital Literacy. Optimised for mobile.</p>"
+        ),
+    }
+
+
+@pytest.mark.anyio
+async def test_main_courses_with_raw(client: AsyncClient):
+    """Test the courses route with `raw=1`."""
+    response = (await client.get("/courses?raw=1")).json()
+    assert len(response) == COURSES_COUNT + 1
+    assert response[0]["fullname"] == "Hook"
+
+
+@pytest.mark.anyio
+async def test_main_courses_with_course_id(client: AsyncClient):
+    """Test the courses/{course_id} route."""
+    response = (await client.get("/courses/3")).json()
+    assert len(response) == 23
+    assert response[0] == {
+        "id": 23,
+        "instance": 3,
+        "name": "Announcements",
+        "modname": "forum",
+        "url": "http://moodle/mod/forum/view.php?id=23",
+        "contents": [],
+    }
+    assert response[18] == {
+        "id": 42,
+        "instance": 2,
+        "name": " Digital Literacies in Higher Ed",
+        "modname": "url",
+        "url": "http://moodle/mod/url/view.php?id=42",
+        "contents": [
+            {
+                "type": "url",
+                "mimetype": "text/html",
+                "fileurl": (
+                    "https://www.heacademy.ac.uk/enhancement/starter-tools/"
+                    "digital-literacies"
+                ),
+                "content": None,
+            }
+        ],
+    }
+
+
+@pytest.mark.anyio
+async def test_main_courses_with_course_id_with_raw(client: AsyncClient):
+    """Test the courses/{course_id} route with `raw=1`."""
+    response = (await client.get("/courses/3?raw=1")).json()
+    assert len(response) == 5
+    assert response[0]["visible"] == 1
+
+
+@pytest.mark.anyio
+async def test_main_quiz_with_quiz_id(client: AsyncClient):
+    """Test the quiz/{quiz_id} route."""
+    response = (await client.get("/quiz/2")).json()
+    assert len(response) == 8
+    assert 'The variable "x" can only contain a number.' in response[0]["html"]
+
+
+@pytest.mark.anyio
+async def test_main_quiz_with_quiz_id_with_raw(client: AsyncClient):
+    """Test the quiz/{quiz_id} route with `raw=1`."""
+    response = (await client.get("/quiz/1?raw=1")).json()
+    assert response["attempt"]["layout"] == "1,2,3,4,5,6,0"
+
+
+def test_main_patch_moodle_url(monkeypatch):
+    """Test the `patch_moodle_url` function."""
+    monkeypatch.setenv("HOOK_MOODLE_URL", "http://foo")
+    assert patch_moodle_url("http://bar:8080/hello/world") == "http://foo/hello/world"
+    assert patch_moodle_url(None) is None
